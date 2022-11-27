e9ac2eb01ea218c7e97e381a94d7c1faa2be5780
willianglitch <nailliw901@gmail.com>
2022-11-27 10:16:31 -0300

    creating the style of the card and create the data

---
+++ src/js/Data.js
@@ -0,0 +1,44 @@
+window.Data = [
+    {
+        name:'Paldean Tauros (fire)',
+        dexNumber:'128',
+        Type01:'fighting',
+        type02:'fire',
+        description:'Quando aquecidos pela energia do fogo, seus chifres podem ficar mais quentes do que 1.800 graus Fahrenheit. Aqueles feridos por eles sofrerão ferimentos e queimaduras.'
+    },
+    {
+        name:'Grafaiai',
+        dexNumber:'203',
+        Type01:'poison',
+        type02:'normal',
+        description:'A cor da saliva venenosa depende do que o Pokémon come. Grafaiai cobre os dedos com saliva e desenha padrões em árvores nas florestas.'
+    },
+    {
+        name:'Tinkaton',
+        dexNumber:'281',
+        Type01:'fairy',
+        type02:'steel',
+        description:'Este Pokémon inteligente tem uma disposição muito ousada. Ele bate pedras no céu com seu martelo, visando acertar Corviknight.'
+    },
+    {
+        name:'Cyclizar',
+        dexNumber:'295',
+        Type01:'dragon',
+        type02:'normal',
+        description:'Aparentemente, a Cyclizar permite que as pessoas andem de costas desde os tempos antigos. Representações disso foram encontradas em murais de 10.000 anos.'
+    },
+    {
+        name:'Skeledirge',
+        dexNumber:'006',
+        Type01:'ghost',
+        type02:'fire',
+        description:'O pássaro de fogo muda de forma quando Skeledirge canta. Há rumores de que o pássaro nasceu quando a bola de fogo na cabeça de Skeledirge ganhou uma alma.'
+    },
+    {
+        name:'Cetitan',
+        dexNumber:'362',
+        Type01:'ice',
+        type02:'----',
+        description:'Este Pokémon vagueia por áreas cobertas de neve e gelo. Ele protege seu corpo com músculos poderosos e uma espessa camada de gordura sob a pele.'
+    }
+]
